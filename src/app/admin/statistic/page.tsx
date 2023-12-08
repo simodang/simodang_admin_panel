29d3f6ce0d9a29bf63cbbd04e9e5e82da43bc557
Aldi-H <aldihimawan88@gmail.com>
2023-12-08 16:29:18 +0700

    SIMD-175 feat: Add Sidebar

---
+++ src/app/admin/statistic/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Statistic = () => {
+  return (
+    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <Text>Statistik Page</Text>
+    </Box>
+  );
+};
+
+export default Statistic;
